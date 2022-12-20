6fba47be58a8e956979273bfb6dbfc210f5aaf6c
DavidRodriguezDev <contacto.davidrodriguezdiaz@gmail.com>
2022-12-20 16:30:34 +0100

    Advanced cocktail detail page without ingredients

--- src/pages/DetailCocktail/DetailCocktail.jsx
+++ src/pages/DetailCocktail/DetailCocktail.jsx
@@ -8,29 +8,46 @@ const DetailCocktail = () => {
 
     const {idCocktail} = useParams();
     const [cocktail, setCocktail] = useState([]);
+    const user = JSON.parse(localStorage.getItem('user')).name;
 
     useEffect(() => {
         const getCocktail = async () => {
             const {data} = await axios.get(`http://localhost:3040/cocktails/${idCocktail}`);
             setCocktail(data);
-            console.log(data);
+
         }
         getCocktail()
     }, [idCocktail])
 
+
   return (
     <div className='b-detailcocktail__container'>
-      <div className='b-register-header'>
-          <BackButton src="../../assets/left.png"></BackButton>
-          <h1 className='b-title'>CHICAGO´S SPEAKEASY</h1>
-          <h3 className='b-opacity'>0</h3>
+        <div className='b-detailcocktail__header'>
+            <BackButton src="../../assets/left.png"></BackButton>
+            <h1 className='b-title'>CHICAGO´S SPEAKEASY</h1>
+            <h3 className='b-opacity'>0</h3>
         </div>  
+        <div className='b-detailcocktail__info'>
+            <div className='b-detailcocktail__image--container'>
+                <img className='b-detailcocktail__image' src={cocktail.photo} alt={cocktail.name}></img>
+            </div>
+            <div className='b-detailcocktail__text'>
+                <h2 className='b-detailcocktail__title'>{cocktail.name}</h2>
+                <div className='b-detailcocktail__mixologist__container'>
+                    <h3 className='b-detailcocktail__mixologist'>MIXOLOGIST :</h3>
+                    <h3 className='b-detailcocktail__mixologist--name'>{user}</h3>
+                </div>       
+            </div>
+            <div className='b-detailcocktail__method--container'>
+                <h3 className='b-detailcocktail__method'>MÉTODO DE ELABORACIÓN : </h3>
+                <h5 className='b-detailcocktail__method--sec'>{cocktail.method}</h5>
+            </div>
+            <div className='b-detailcocktail__ingredients'>
+                <h3>INGREDIENTS :</h3>
+                
+            </div>
+        </div>
         
-        
-        <h1>{cocktail.name}</h1>
-        <h1>{cocktail.glass}</h1>
-        <h1>{cocktail.ice}</h1>
-        <h1>{cocktail.method}</h1>  
     </div>
   )
 }
